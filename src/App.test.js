9452d3948b3b2eedac3de69c5caa03147085c97c
Anzunomi <133649718+sexymanalive@users.noreply.github.com>
2026-01-03 16:51:30 +0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -20,7 +20,7 @@ describe('Routing Test About Us', () => {
       </MemoryRouter>
     ); 
     // Assert 
-    expect(screen.getByText(/About Our Team/i)).toBeInTheDocument();
+    expect(screen.getByText(/About Our Team2/i)).toBeInTheDocument();
 
   })
 
